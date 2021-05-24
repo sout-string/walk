f21dd47cd0985231138cfaaeb6ba813ab6a975d0
Wangxiaoshuai <1429600159@qq.com>
2021-05-24 10:21:25 +0800

    提交问卷用到的接口

--- tanhua-dubbo/tanhua-dubbo-service/src/main/java/com/tanhua/dubbo/mapper/QuestionoptionsMapper.java
+++ tanhua-dubbo/tanhua-dubbo-service/src/main/java/com/tanhua/dubbo/mapper/QuestionoptionsMapper.java
@@ -2,11 +2,11 @@ package com.tanhua.dubbo.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.tanhua.domain.db.Questionoptions;
-import org.apache.ibatis.annotations.Mapper;
 
 /**
- *  选项
+ * @description
+ * @author:Dell
+ * @date:2021/5/23 13:12
  */
-@Mapper
-public interface QuestionoptionsMapper extends BaseMapper<Questionoptions> {
+public interface QuestionOptionsMapper extends BaseMapper<Questionoptions> {
 }
